e52ef1b8ddf18c7f5b61ffca7d53206b389afddf
Carol <94094252+Car00l@users.noreply.github.com>
2022-09-27 08:47:49 -0300

    Update Select-Projeto.sql

--- Projetos/Select-Projeto.sql
+++ Projetos/Select-Projeto.sql
@@ -1,37 +1,37 @@
 
--- Questa A
+-- Listar todos os alunos da cidade de Lins.
 select *
 from aluno
 where cidade = 'Lins';
 
--- Questao B
+-- Listar todos alunos que fazem o curso Técnico em Informática.
 select *
 from aluno
 where curso_id = 1;
 
--- Questao C
+-- Listar todos alunos que possuem idade superior a 18 anos.
 select *
 from aluno
 where idade > 18;
 
--- Questao D
+-- Listar todos alunos que possuem idade entre 18 e 35 anos.
 select *
 from aluno
 where idade BETWEEN 18 AND 35; 
 
--- Questao E
+-- Listar os alunos que fazem o curso Técnico em Recursos Humanos e são da cidadede Getulina.
 select *
 from aluno
 where curso_id = 3
 and cidade = 'Getulina';
 
--- Quetao F
+-- Listar os alunos que fazem o curso Técnico em Recursos Humanos que são da cidade de Lins ou Guaimbê.
 select *
 from aluno
 where curso_id = 3
 where cidade = 'Guaimbe'
 
--- Questao G
+-- Listar id, matrícula do aluno e área do curso de todos os alunos que fazem Técnico em Informática.
 select aluno.aluno_id, curso.area, aluno.matricula
 from aluno, curso
 where aluno.curso_id = 1
